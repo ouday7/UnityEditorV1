ff8da260a3fc60b8ac35c9eac4618e17ad2d377f
bahaeddine hedyaoui <67714398+Baha0007@users.noreply.github.com>
2022-01-11 11:44:40 +0100

    mainContent update Size dynamix

--- Assets/Scripts/ChapterPanel/ObjectsHolder.cs
+++ Assets/Scripts/ChapterPanel/ObjectsHolder.cs
@@ -8,10 +8,13 @@ public class ObjectsHolder : EntryPointSystemBase
     [SerializeField] private ObjectsHolder holder;
     [SerializeField] private RectTransform quizfieldsHolder;
     private RectTransform _rt;
-    private float HeightToAdd = 400;
+    private float HeightToAdd = 150;
+    private Vector2 holderSize;
     public override void Begin()
     {
+        holderSize = holder.RectTransform.sizeDelta;
         SelectTemplateDialog.instance.OnSubmitTemplate += MaximiseHolderHeight;
+        QuestionBtn.OnClickQuestion += MaximiseHolderQuestionClick;
     }
     private RectTransform RectTransform
     {
@@ -21,10 +24,25 @@ public class ObjectsHolder : EntryPointSystemBase
             return _rt;
         }
     }
-
     private void MaximiseHolderHeight(TemplateData inTemplate)
     {
-        var holderSize = holder.RectTransform.sizeDelta;
-        holder.RectTransform.sizeDelta = new Vector2(holderSize.x, holderSize.y + HeightToAdd);
+        var quizfieldChilds = quizfieldsHolder.childCount;
+        if(quizfieldChilds==0)
+        {
+            holder.RectTransform.sizeDelta=holderSize;
+            return;
+        }
+        holder.RectTransform.sizeDelta = new Vector2(holderSize.x, holderSize.y +(quizfieldChilds* HeightToAdd));
     }
+    private void MaximiseHolderQuestionClick(QuestionBtn btn)
+    {
+        var quizfieldChilds = quizfieldsHolder.childCount;
+        
+        if(quizfieldChilds==0)
+        {
+          holder.RectTransform.sizeDelta=holderSize;
+          return;
+        }
+        holder.RectTransform.sizeDelta = new Vector2(holderSize.x, holderSize.y +(quizfieldChilds* HeightToAdd));
+    } 
 }
