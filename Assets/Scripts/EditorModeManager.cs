e6839bbcd58938f4ef6720fc75793258421e3f70
Ouday El Akremi <51921339+ouday7@users.noreply.github.com>
2022-01-17 13:04:04 +0100

    update

--- Assets/Scripts/EditorModeManager.cs
+++ Assets/Scripts/EditorModeManager.cs
@@ -5,101 +5,108 @@ using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.UI;
 using UPersian.Components;
+
 public class EditorModeManager : MonoBehaviour
 {
-        public static event Action<QuestionData> OnDesignClick;
-        public static event Action<TemplateBase,QuestionData> OnGetTemplateComplete; 
+    public static event Action<QuestionData> OnDesignClick;
+    public static event Action<TemplateBase, QuestionData> OnGetTemplateComplete;
 
-        [BoxGroup("Unselected Mode")][LabelText("Color")] [SerializeField] private Color unselectedColor;
-        [BoxGroup("Selected Mode")][LabelText("Color")] [SerializeField] private Color selectedColor;
-        
-        [SerializeField] private GameObject designmodePanel;
-        [SerializeField] private Button designmodeButton;
-        [SerializeField] private Button editmodeButton;
-        [SerializeField] private GameObject warningPanel;
-        [SerializeField] private Button HidewarningPanelBtn;
-        [SerializeField] private GameObject gameplayPanel;
-    
-        private Button _btn;
-        private Image _bg;
-        private QuestionData currentQstData;
-        
+    [BoxGroup("Unselected Mode")] [LabelText("Color")] [SerializeField]
+    private Color unselectedColor;
 
-        private void Awake()
-        {
-            designmodeButton.onClick.AddListener(ClickDesignMode);
-            editmodeButton.onClick.AddListener(OnClickEditMode);
-            HidewarningPanelBtn.onClick.AddListener(()=>warningPanel.SetActive(false));
-            OnDesignClick += OnClickDesignMode;
-            QuestionBtn.OnClickQuestion += PrepareDesignMode;
-            OnGetTemplateComplete += ShowTemplateGamePlay;
-        }
+    [BoxGroup("Selected Mode")] [LabelText("Color")] [SerializeField]
+    private Color selectedColor;
 
-        private void ShowTemplateGamePlay(TemplateBase obj,QuestionData currentQuestion)
-        {
-            obj.Initialize(currentQuestion);
-        }
-        private void PrepareDesignMode(QuestionBtn questionButton)
-        {
-            if (questionButton.Data.quizFields == null)
-            {
-                /*foreach (Transform obj in gameplayPanel.transform)
-                {
-                    Destroy(obj);                  
-                }*/
-                return;
-            }
-           var currentQstData=questionButton.Data;
-           var templateId = currentQstData.templateId;
-           GetTemplate(templateId,currentQstData);
+    [SerializeField] private GameObject designmodePanel;
+    [SerializeField] private Button designmodeButton;
+    [SerializeField] private Button editmodeButton;
+    [SerializeField] private GameObject warningPanel;
+    [SerializeField] private Button HidewarningPanelBtn;
+    [SerializeField] private GameObject gameplayPanel;
 
-        }
-        private void GetTemplate(int templateId,QuestionData currentQuestionData)
-        {
-            var type = (TemplatesNames) templateId;
-            Debug.Log(type.ToString());
-            var prefab = TemplatesHandler.Instance.templatesCatalog.FirstOrDefault(obj => obj.type==type).prefab;
-            if (prefab == null)
-            {
-                Debug.Log("Prefab is null !");
-                return;
-            }
-            var template = Instantiate(prefab, gameplayPanel.transform).gameObject.GetComponent<TemplateBase>();
-            template.gameObject.GetComponent<RectTransform>().localScale = Vector3.one;
-            OnGetTemplateComplete?.Invoke(template,currentQuestionData);
-        }
+    private Button _btn;
+    private Image _bg;
+    private QuestionData currentQstData;
 
-        private void ClickDesignMode()
-        {
-            OnDesignClick?.Invoke(EditManager.Instance.currentQuestionData);
-        }
-    
-        private void OnClickDesignMode(QuestionData questionData)
-        {
-            if (questionData.quizFields.Count == 0)
-            {
-                warningPanel.SetActive(true);
-                return;
-            }
-            var templateId = questionData.templateId;
-            GetTemplate(templateId,questionData);
-            designmodePanel.gameObject.SetActive(true);
-            designmodeButton.interactable = false;
-            designmodeButton.GetComponent<Image>().color = selectedColor;
-            editmodeButton.interactable = true;
-            editmodeButton.GetComponent<Image>().color = unselectedColor;
-        }
-        
-        private void OnClickEditMode()
-        {
-            designmodePanel.gameObject.SetActive(false);
-            
-            editmodeButton.interactable = false;
-            editmodeButton.GetComponent<Image>().color = selectedColor;
-            
-            designmodeButton.interactable = true;
-            designmodeButton.GetComponent<Image>().color = unselectedColor;
-        }
 
-      
+    private void Awake()
+    {
+        designmodeButton.onClick.AddListener(ClickDesignMode);
+        editmodeButton.onClick.AddListener(OnClickEditMode);
+        HidewarningPanelBtn.onClick.AddListener(() => warningPanel.SetActive(false));
+        OnDesignClick += OnClickDesignMode;
+        QuestionBtn.OnClickQuestion += PrepareDesignMode;
+        OnGetTemplateComplete += ShowTemplateGamePlay;
     }
+
+    private void ShowTemplateGamePlay(TemplateBase obj, QuestionData currentQuestion)
+    {
+        obj.Initialize(currentQuestion);
+    }
+
+    private void PrepareDesignMode(QuestionBtn questionButton)
+    {
+        if (questionButton.Data.quizFields == null)
+        {
+            /*foreach (Transform obj in gameplayPanel.transform)
+            {
+                Destroy(obj);                  
+            }*/
+            return;
+        }
+
+        var currentQstData = questionButton.Data;
+        var templateId = currentQstData.templateId;
+        GetTemplate(templateId, currentQstData);
+    }
+
+    private void GetTemplate(int templateId, QuestionData currentQuestionData)
+    {
+        var type = (TemplatesNames) templateId;
+        Debug.Log(type.ToString());
+        var prefab = TemplatesHandler.Instance.templatesCatalog.FirstOrDefault(obj => obj.type == type).prefab;
+        Debug.Log(prefab.name+"prefaab name");
+        if (prefab == null)
+        {
+            Debug.Log("Prefab is null !");
+            return;
+        }
+
+        var template = Instantiate(prefab, gameplayPanel.transform).gameObject.GetComponent<TemplateBase>();
+        template.gameObject.GetComponent<RectTransform>().localScale = Vector3.one;
+        OnGetTemplateComplete?.Invoke(template, currentQuestionData);
+    }
+
+    private void ClickDesignMode()
+    {
+        OnDesignClick?.Invoke(EditManager.Instance.currentQuestionData);
+    }
+
+    private void OnClickDesignMode(QuestionData questionData)
+    {
+        if (questionData.quizFields.Count == 0)
+        {
+            warningPanel.SetActive(true);
+            return;
+        }
+
+        var templateId = questionData.templateId;
+        GetTemplate(templateId, questionData);
+        designmodePanel.gameObject.SetActive(true);
+        designmodeButton.interactable = false;
+        designmodeButton.GetComponent<Image>().color = selectedColor;
+        editmodeButton.interactable = true;
+        editmodeButton.GetComponent<Image>().color = unselectedColor;
+    }
+
+    private void OnClickEditMode()
+    {
+        designmodePanel.gameObject.SetActive(false);
+
+        editmodeButton.interactable = false;
+        editmodeButton.GetComponent<Image>().color = selectedColor;
+
+        designmodeButton.interactable = true;
+        designmodeButton.GetComponent<Image>().color = unselectedColor;
+    }
+}
--- Assets/Scripts/Templates/TrueFalseTemplate.cs
+++ Assets/Scripts/Templates/TrueFalseTemplate.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 using UPersian.Components;
 using DG.Tweening;
+using Unity.VisualScripting;
 
 public class TrueFalseTemplate : TemplateBase
 {
-    [SerializeField] private QuestionData questionData;
     [SerializeField] private RtlText questionTxt;
     [SerializeField] private Image imageTxt;
     [SerializeField] private Button btnA;
     [SerializeField] private Button btnB;
-    [SerializeField] private VerticalLayoutGroup contentHolder;
-    [SerializeField] private HorizontalLayoutGroup btnHolder;
+    private QuestionData questionData;
     private bool _result, _a, _b;
 
 
@@ -36,8 +35,11 @@ public class TrueFalseTemplate : TemplateBase
         _result = false;
         imageTxt.sprite = questionData.quizFields[0].imageOne;
         questionTxt.text = questionData.quizFields[0].textA;
-        btnA.name = questionData.quizFields[1].textA;
-        btnB.name = questionData.quizFields[2].textA;
+        
+        btnA.GetComponentInChildren<RtlText>().text = questionData.quizFields[1].textA;
+        btnB.GetComponentInChildren<RtlText>().text = questionData.quizFields[2].textA;
+
+    
         btnA.onClick.AddListener(OnClickButtonA);
         btnB.onClick.AddListener(OnClickButtonB);
 
@@ -45,12 +47,12 @@ public class TrueFalseTemplate : TemplateBase
 
     private void OnClickButtonA()
     {
-        btnA.Select();
+     
         _a = questionData.quizFields[1].toggleA;
         _result = _a;
         btnA.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
         transform.DOScale(Vector3.one, 0.15f).SetEase(Ease.InOutBack);
-
+        
         btnB.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
         transform.DOScale(Vector3.one, 0.15f).SetEase(Ease.InOutBack);
         btnA.interactable = false;
@@ -59,7 +61,6 @@ public class TrueFalseTemplate : TemplateBase
 
     private void OnClickButtonB()
     {
-        btnB.Select();
         _b = questionData.quizFields[2].toggleA;
         _result = _b;
         btnB.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
